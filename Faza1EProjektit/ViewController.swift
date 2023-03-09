b609f3065d1862ab71f75058087dab161a5d07e7
Shqiperim Ramadani <shqipi_r@hotmail.com>
2023-03-09 21:51:59 +0100

    Initial Commit

---
+++ Faza1EProjektit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Faza1EProjektit
+//
+//  Created by shqiperimramadani on 09.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
